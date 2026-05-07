147dc186e400848978ade558c8bb2ba3a2f500b9
AlmeidaKevin <almeidakevin783@gmail.com>
2026-05-07 15:09:37 -0500

    Login
    Login

---
+++ loginsupabase-main/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://vyungyoknkcjbvfiifjr.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dW5neW9rbmtjamJ2ZmlpZmpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzU1ODMsImV4cCI6MjA5Mzc1MTU4M30.DjlWXdKX1jkp3VLlF_colGBWAxsEWesbQkSbBSMhSHA'
+};
